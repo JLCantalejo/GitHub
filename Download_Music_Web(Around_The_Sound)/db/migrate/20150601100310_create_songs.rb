5c0ea7839c783345bc63e5fe443f8648afb32f8d
JLCantalejo <joseluiscantalejo@hotmail.com>
2015-06-01 12:28:31 +0200

    Migrate y vista-método index

--- Download_Music_Web(Around_The_Sound)/db/migrate/20150601100310_create_songs.rb
+++ Download_Music_Web(Around_The_Sound)/db/migrate/20150601100310_create_songs.rb
@@ -1,7 +1,9 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-
+    	t.string :artists
+    	t.string :tracks
+    	t.string :albums
       t.timestamps null: false
     end
   end
