7ab1b35a4a7e755f110f24b2e60709e68d85e3dd
JLCantalejo <joseluiscantalejo@hotmail.com>
2015-06-01 12:47:21 +0200

    Rails creado

--- Download_Music_Web(Around_The_Sound)/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  get '/' => 'songs#index', as: 'root'
-  resources :songs
-end
---
+++ Download_Web_Music(Around_The_Sound)/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_Download_Web_Music(Around_The_Sound)_session'
