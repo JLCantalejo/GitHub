ecb435274326478e348a361f035fa1d427ca9bc5
JLCantalejo <joseluiscantalejo@hotmail.com>
2015-06-02 12:57:53 +0200

    Vista y controladores de albums

--- Download_Web_Music(Around_The_Sound)/config/routes.rb
+++ Download_Web_Music(Around_The_Sound)/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'artists#index', as: 'root'
+  get '/artists/albums' => 'albums#view_albums'
   resources :artists do
     resources :albums do
     resources :songs
