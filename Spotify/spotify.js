5714d0f194dc5e830e49afcb355591bd17110f6a
JLCantalejo <joseluiscantalejo@hotmail.com>
2015-06-03 18:10:45 +0200

    pruebas reproductor audio

--- Spotify/spotify.js
+++ Spotify/spotify.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$(".buscar").on("click", function(){
 		var art =  $("#artist").val()
 		
-		console.log(art);
+		//console.log(art);
 			$.ajax({
 			type: "GET",
 			url: "https://api.spotify.com/v1/search?type=album&q=" + art,
@@ -12,17 +12,40 @@ $(document).ready(function(){
 			dataType: "json",
 		});
 	});
-
+		var idAlbum;
 		function the_artist(element){
 			/*var list = element.artists;
 			var items = list.items;
 			var images = items.images;
 			var url_images = images.url_images;*/
-			$("ul").empty();
-			total_length = element.tracks.items.length;
-					console.log(element);
-				
-				
+			
+			total_length = element.albums.items.length;
+				//console.log(element);
+				for(var i = 0; i < total_length; i++){
+				idAlbum = element.albums.items[i].id;
+				//console.log(idAlbum);
+				getAlbumsFromArtist(idAlbum);
+			};
+		};
+
+		function getAlbumsFromArtist(idAlbum) {
+			$.ajax({
+				type: "GET",
+				url: "https://api.spotify.com/v1/albums/" + idAlbum,
+				data: "",
+				success: function(response){the_album(response)},
+				error: function(){console.log("Error")},
+				dataType: "json"
+			});
+		};
+		function the_album(element2){
+			$('ul').empty();
+			total_length = element2.tracks.items.length;
+			console.log(element2);
+			for(var i = 0; i < total_length; i++){
+			$('ul').append("<li>" + element2.tracks.items[i].name + "</li>");
+
+		};
 		};
 /*
 	$(".buscar").on("click", function(){
