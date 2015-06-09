1d9ccc8f3ce361019c0cf2fa63d3f5e4b7768810
JLCantalejo <joseluiscantalejo@hotmail.com>
2015-06-09 16:19:33 +0200

    se muestran los albums del artista concreto

--- Download_Web_Music(Around_The_Sound)/app/controllers/albums_controller.rb
+++ Download_Web_Music(Around_The_Sound)/app/controllers/albums_controller.rb
@@ -1,16 +1,12 @@
 class AlbumsController < ApplicationController
 	def index
-		@artist = Artist.find params[:artist_id]
-		@albums = @artist.albums
-	end
-	def show
-
-
-		artist_id = params[:id]
+		artist_id = params[:artist_id]
 		artist_spotify = RSpotify::Artist.find(artist_id)
 		@albums = artist_spotify.albums
-		
-		
+		binding.pry
+
+	end
+	def show
 	end
 	def view_albums
 		@albums = Album.all
--- Download_Web_Music(Around_The_Sound)/app/controllers/artists_controller.rb
+++ Download_Web_Music(Around_The_Sound)/app/controllers/artists_controller.rb
@@ -1,10 +1,9 @@
 class ArtistsController < ApplicationController
 	def index
-		@artists = Artist.all
 	end
 	def show
-		artist_id = params[:id]
-		artist_spotify = RSpotify::Artist.find(artist_id)
-		@artists = artist_spotify
+		@artist_id = params[:id]
+		artist_spotify = RSpotify::Artist.find(@artist_id)
+		@artist = artist_spotify
 	end
 end
