03257f45c4e3dac158ef3a21fd5a7fe09a29b074
JLCantalejo <joseluiscantalejo@hotmail.com>
2015-06-10 15:07:35 +0200

    se ven los albumes del artista con sus canciones

--- Download_Web_Music(Around_The_Sound)/app/controllers/albums_controller.rb
+++ Download_Web_Music(Around_The_Sound)/app/controllers/albums_controller.rb
@@ -3,11 +3,13 @@ class AlbumsController < ApplicationController
 		artist_id = params[:artist_id]
 		artist_spotify = RSpotify::Artist.find(artist_id)
 		@albums = artist_spotify.albums
+		
 		binding.pry
-
 	end
+
 	def show
 	end
+
 	def view_albums
 		@albums = Album.all
 	end
